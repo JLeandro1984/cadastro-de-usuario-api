2bdb2717b8c3a0d1025ff0f6f0c2d64ced421137
José Leandro <43589787+JLeandro1984@users.noreply.github.com>
2024-06-25 22:19:28 -0300

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './style.css'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Olá JL 123</h1>    
+    </>
+  )
+}
+
+export default Home
